d3293632262f12878c27e9f511eb87a23ee6fe70
Ruben Danielyan <ruben.danielyan@kingfisher.com>
2022-11-19 22:35:09 +0400

    Updated test after rebase

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import App from "./App"
 
 test("renders temporary main page", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Goal management/i)
-  expect(linkElement).toBeInTheDocument()
+  const { container } = render(<App />)
+  expect(container).toBeEmptyDOMElement()
 })
