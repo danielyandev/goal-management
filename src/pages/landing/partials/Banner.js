a63d99d5967c996893a8edf768b0fa81fcd793db
Ruben Danielyan <ruben.danielyan@kingfisher.com>
2022-11-20 22:31:03 +0400

    Add Banner test

--- src/pages/landing/partials/Banner.js
+++ src/pages/landing/partials/Banner.js
@@ -4,10 +4,13 @@ function Banner() {
   return (
     <div className="d-flex flex-column justify-content-center w-100 min-height-500 px-9">
       <div className="text-center mb-5 mb-lg-10 py-10 py-lg-20">
-        <h1 className="text-white lh-base fw-bolder fs-2x fs-lg-3x mb-15">
+        <h1
+          className="text-white lh-base fw-bolder fs-2x fs-lg-3x mb-15"
+          data-testid="banner-title"
+        >
           Achieve Better Results
           <br />
-          With
+          <span> With </span>
           <span className="landing-title-gradient">
             <span>The Best Goal Management System Ever</span>
           </span>
