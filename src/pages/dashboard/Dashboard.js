8794a3b3e85b61a458d98226453c96d3b3dc8251
Ruben Danielyan <ruben.danielyan@kingfisher.com>
2022-11-28 21:27:19 +0400

    Replace test data with dummy fetch method

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -8,35 +8,13 @@ function Dashboard() {
   const [goals, setGoals] = useState([])
 
   useEffect(() => {
-    // todo fetch goals
-    setGoals([
-      {
-        id: 1,
-        title: "test title",
-        description: "test description",
-        assignee: "test assignee",
-        createdAt: "Today",
-        status: "pending"
-      },
-      {
-        id: 2,
-        title: "test title",
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        assignee: "test assignee",
-        createdAt: "Today",
-        status: "approved"
-      },
-      {
-        id: 3,
-        title: "test title",
-        description: "test description",
-        assignee: "test assignee",
-        createdAt: "Today",
-        status: "rejected"
-      }
-    ])
+    fetchGoals()
   }, [])
+
+  // todo fetch
+  const fetchGoals = () => {
+    setGoals([])
+  }
 
   const renderGoals = () => {
     if (!goals.length) {
