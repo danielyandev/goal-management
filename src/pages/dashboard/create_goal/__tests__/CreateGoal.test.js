79822c33a57b2669a06339d7ff167d4f1dbf1c98
Ruben Danielyan <ruben.danielyan@kingfisher.com>
2022-12-04 18:48:33 +0400

    CreateGoal test fix

--- src/pages/dashboard/create_goal/__tests__/CreateGoal.test.js
+++ src/pages/dashboard/create_goal/__tests__/CreateGoal.test.js
@@ -1,14 +1,27 @@
-import { fireEvent, render } from "@testing-library/react"
+import { act, fireEvent, render } from "@testing-library/react"
 import CreateGoal from "../CreateGoal"
 import TestsWrapper from "../../../../helpers/TestsWrapper"
 import * as goals from "../../../../api/goals"
 
 jest.spyOn(goals, "createGoal").mockResolvedValueOnce({})
-
+jest.mock("axios", () => {
+  return {
+    get: () => {
+      return Promise.resolve({
+        data: [{ username: "testuser", fullName: "Test User" }]
+      })
+    }
+  }
+})
 describe("CreateGoal component", () => {
-  test("renders component", () => {
-    const { queryByText, getByTestId } = render(<CreateGoal />, {
-      wrapper: TestsWrapper
+  test("renders component", async () => {
+    let queryByText, getByTestId
+    await act(() => {
+      const res = render(<CreateGoal />, {
+        wrapper: TestsWrapper
+      })
+      getByTestId = res.getByTestId
+      queryByText = res.queryByText
     })
     expect(queryByText("Create goal")).toBeInTheDocument()
     expect(getByTestId("title-field")).toBeInTheDocument()
@@ -17,12 +30,18 @@ describe("CreateGoal component", () => {
     expect(getByTestId("create-button")).toBeInTheDocument()
   })
 
-  test("request is not sent on create when validation is not passed", () => {
-    const { getByTestId, queryByTestId } = render(<CreateGoal />, {
-      wrapper: TestsWrapper
+  test("request is not sent on create when validation is not passed", async () => {
+    let getByTestId, queryByTestId
+    await act(() => {
+      const res = render(<CreateGoal />, {
+        wrapper: TestsWrapper
+      })
+      getByTestId = res.getByTestId
+      queryByTestId = res.queryByTestId
     })
 
     fireEvent.click(getByTestId("create-button"))
+
     expect(goals.createGoal).toBeCalledTimes(0)
     expect(queryByTestId("errors")).toBeInTheDocument()
   })
